9ec5b6aed7ddfbad0cb88a36f21dc80ba130ba57
unknown <bojandavid98@gmail.com>
2022-12-29 16:57:29 +0200

    updates

--- cmd
+++ cmd
@@ -5,3 +5,6 @@ pip install webdriver-manager
 
 run test
 behave -f html -o behave-report.html
+
+run tests with tags: homepage, search, login, cart
+behave -f html -o behave-report.html --tags=search
--- pages/products_page.py
+++ pages/products_page.py
@@ -8,7 +8,7 @@ class ProductsPage(BasePage):
 
     def verify_results_contain_text(self, text):
         products_list = self.driver.find_elements(By.PARTIAL_LINK_TEXT, text)
-        for i in range(3):
+        for i in range(2):
             title = products_list[i].text.lower()
             self.assertTrue(text in title, 'Result does not contain search query')
 
